0d2a3ec6a190220d038728ad8bfc6a526dfd69cd
chargeflux <20979457+chargeflux@users.noreply.github.com>
2021-10-31 14:22:50 -0400

    Logging: Fix naming

--- src/services/logger-service.ts
+++ src/services/logger-service.ts
@@ -18,7 +18,7 @@ export function rotateLog() {
   logger.info("Rotating log");
   const date = new Date();
   const dateString = [
-    date.getDay().toString().padStart(2, "0"),
+    date.getDate().toString().padStart(2, "0"),
     date.getMonth().toString().padStart(2, "0"),
     date.getFullYear(),
   ].join("-");
